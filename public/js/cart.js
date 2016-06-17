fcd39c67cfa36c72f85822be8b2665545eeb32aa
Harry Boyes <harry.boyes@gmail.com>
2016-06-17 20:23:21 +0100

    Added jumping

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,4 +1,4 @@
-var Cart = function(){
+var Cart = function(controls){
   var that = this;
 
   this.sprite = game.add.sprite(400, 460, 'cart');
@@ -7,14 +7,43 @@ var Cart = function(){
   this.sprite.angle = 1.5;
 
   var angleVelocity = 0;
+  var yVelocity = 0;
+
+  var jump = function(){
+    yVelocity = -10;
+  }
+
+  controls.jump.onDown.add(jump, this);
 
   this.update = function(){
-    if(that.sprite.angle >= 0){
-      angleVelocity -= 0.25;
-    } else {
-      angleVelocity += 0.25;
-    }
+    if(yVelocity > 0 && this.sprite.y == 460){
+      if(that.sprite.angle <= -1.5){
+        if(that.sprite.angle >= 0){
+          angleVelocity -= 0.75;
+        } else {
+          angleVelocity += 0.75;
+        }
+      } else {
+        if(that.sprite.angle >= 0){
+          angleVelocity -= 0.25;
+        } else {
+          angleVelocity += 0.25;
+        }
 
-    that.sprite.angle += angleVelocity;
+        if(Math.abs(angleVelocity) > 0.75)
+          angleVelocity = (angleVelocity / Math.abs(angleVelocity)) * 0.75;
+      }
+
+      that.sprite.angle += angleVelocity;
+    } else {
+      that.sprite.y += yVelocity;
+      yVelocity += 0.5;
+
+      that.sprite.angle = -30;
+
+      if(that.sprite.y >= 460) {
+        that.sprite.y = 460;
+      }
+    }
   }
 }
