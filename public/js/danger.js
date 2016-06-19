1f2321d7cea5cb224b1f43fa8c4a350991d07c76
Harry Boyes <harry.boyes@gmail.com>
2016-06-19 17:36:27 +0100

    Introduce danger spikes
    Introduce danger spikes that can appear instead of coins
    and to start with do nothing when colliding with the player

---
+++ public/js/danger.js
@@ -0,0 +1,19 @@
+var Danger = function(removeCallback){
+  var that = this;
+  this.sprite = game.add.sprite(800, 310, 'danger');
+
+  this.hitBox = function(){
+    return new Phaser.Rectangle(that.sprite.x + 15, that.sprite.y + 10, that.sprite.width - 40, that.sprite.height - 20);
+  }
+
+  this.collide = function(){
+    that.sprite.destroy();
+  }
+
+  this.update = function(){
+    that.sprite.x -= 400 * game.time.elapsed/1000;
+
+    if(that.sprite.x < -that.sprite.width)
+      removeCallback();
+  }
+}
